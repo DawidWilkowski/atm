8217c200417fea54beaef6ae07f1627f3e28b084
DawidWilkowski <dawid.wilkowski98@gmail.com>
2022-11-11 18:50:21 +0100

    Tweaks and password support

--- src/main/java/atm/Main.java
+++ src/main/java/atm/Main.java
@@ -7,113 +7,90 @@ import java.util.Scanner;
 public class Main {
 
 
-	public static void main(String[] args) throws SQLException, ClassNotFoundException {
-		Database database = new Database();
-		Connection connect = DriverManager.getConnection(database.getURL(), database.getUSERNAME(), database.getPASSWORD());
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        Main main = new Main();
+        Statement statement = main.databaseConnect();
+        Scanner scannerObject = new Scanner(System.in);
+		System.out.println("Enter Login: ");
+        String login = scannerObject.nextLine();
+		System.out.println("Enter Password: ");
+		String password = scannerObject.nextLine();
 
-		Statement statement = 	connect.createStatement();
-		Scanner scannerObject = new Scanner(System.in);
-		String userName = scannerObject.nextLine();
+        ResultSet resultSet = statement.executeQuery("SELECT * FROM account WHERE login = '" + login + "' AND password = '" + password + "'");
+        if (resultSet.next() == true) {
+            System.out.println("Hello " + resultSet.getString("login"));
+            main.initializePage();
+            while (true) {
+                main.inputFromUser(resultSet);
+            }
+        } else {
+            throw new RuntimeException("Wrong username or password");
+        }
+    }
 
-		ResultSet resultSet = statement.executeQuery("SELECT * FROM account WHERE name = '" + userName + "'");
-
-		if(resultSet.next() == true){
-			System.out.println("Hello " + resultSet.getString("name"));
-			Main main = new Main();
-			main.initializePage();
-			while (true) {
-				main.inputFromUser(resultSet);
-			}
-		}
-		else {
-			throw new RuntimeException("There is no acccount named:" + userName );
-		}
-
-		
-
-		
-
-
-	}
-
-
-	private void initializePage() {
-		System.out.println("International Bank of Poland");
-		System.out.println("How can we help You?");
-		System.out.println("1 - Balance");
-		System.out.println("2 - Deposit");
-		System.out.println("3 - Withdraw");
-	}
-
-	private void inputFromUser(ResultSet resultSet) throws SQLException {
-		Scanner scannerObject = new Scanner(System.in);
-		int userInput = scannerObject.nextInt();
-		switch (userInput) {
-		case 1: {
-			Database database = new Database();
-			Connection connect = DriverManager.getConnection(database.getURL(), database.getUSERNAME(), database.getPASSWORD());
-			System.out.println("\f");
-			System.out.println("1 - Balance");
-			Statement statement = connect.createStatement();
-			ResultSet rs = statement.executeQuery("SELECT * FROM account WHERE name = '" + resultSet.getString("name") + "'" );
-			rs.next();
-			System.out.println("You have:" + rs.getFloat("amount") + "zl \n");
-
-			initializePage();
-			break;
-		}
-		case 2: {
-			Database database = new Database();
-			Connection connect = DriverManager.getConnection(database.getURL(), database.getUSERNAME(), database.getPASSWORD());
-
-			System.out.println("Enter amount:");
-			int userDepositAmount = scannerObject.nextInt();
-
-			Statement statement = connect.createStatement();
-			ResultSet result = statement.executeQuery("SELECT * FROM account WHERE name = '" + resultSet.getString("name") + "'" );
-			result.next();
-			int amountToAdd = result.getInt("amount") + userDepositAmount;
-			statement.executeUpdate("UPDATE account SET amount = " + amountToAdd );
-
-			ResultSet rs = statement.executeQuery("SELECT * FROM account WHERE name = '" + resultSet.getString("name") + "'" );
-			rs.next();
-			System.out.println("Successfully added " + userDepositAmount + "zl to Your account");
-			System.out.println("You have:" + rs.getInt("amount") + "zl \n");
-
-			initializePage();
-			break;
-		}
-		case 3: {
-			Database database = new Database();
-			Connection connect = DriverManager.getConnection(database.getURL(), database.getUSERNAME(), database.getPASSWORD());
-
-			System.out.println("Enter amount:");
-			Statement statement = connect.createStatement();
-			ResultSet result = statement.executeQuery("SELECT * FROM account WHERE name = '" + resultSet.getString("name") + "'" );
-			result.next();
-			int userWithdrawalAmount = scannerObject.nextInt();
-			if (userWithdrawalAmount <= result.getInt("amount")) {
+    private Statement databaseConnect() throws SQLException {
+        Database database = new Database();
+        Connection connect = DriverManager.getConnection(database.getURL(), database.getUSERNAME(), database.getPASSWORD());
+        Statement statement = connect.createStatement();
+        return statement;
+    }
 
 
-				int amountToSubstract = result.getInt("amount") - userWithdrawalAmount;
-				statement.executeUpdate("UPDATE account SET amount = " + amountToSubstract );
+    private void initializePage() {
+        System.out.println("International Bank of Poland");
+        System.out.println("How can we help You?");
+        System.out.println("1 - Balance");
+        System.out.println("2 - Deposit");
+        System.out.println("3 - Withdraw");
+    }
+
+    private void inputFromUser(ResultSet resultSet) throws SQLException {
+        Scanner scannerObject = new Scanner(System.in);
+        int userInput = scannerObject.nextInt();
+        switch (userInput) {
+            case 1: {
+                Statement statement = databaseConnect();
+                System.out.println("1 - Balance");
+                ResultSet rs = statement.executeQuery("SELECT * FROM account WHERE login = '" + resultSet.getString("login") + "'");
+                rs.next();
+                System.out.println("You have:" + rs.getFloat("amount") + "zl \n");
+                initializePage();
+                break;
+            }
+            case 2: {
+                System.out.println("Enter amount:");
+                int userDepositAmount = scannerObject.nextInt();
+                Statement statement = databaseConnect();
+                ResultSet result = statement.executeQuery("SELECT * FROM account WHERE login = '" + resultSet.getString("login") + "'");
+                result.next();
+                int amountToAdd = result.getInt("amount") + userDepositAmount;
+                statement.executeUpdate("UPDATE account SET amount = " + amountToAdd);
+                System.out.println("Successfully added " + userDepositAmount + "zl to Your account");
+                System.out.println("You have:" + amountToAdd + "zl \n");
+                initializePage();
+                break;
+            }
+            case 3: {
+                System.out.println("Enter amount:");
+                int userWithdrawalAmount = scannerObject.nextInt();
+                Statement statement = databaseConnect();
+                ResultSet result = statement.executeQuery("SELECT * FROM account WHERE login = '" + resultSet.getString("login") + "'");
+                result.next();
+                if (userWithdrawalAmount <= result.getInt("amount")) {
+                    int amountToSubstract = result.getInt("amount") - userWithdrawalAmount;
+                    statement.executeUpdate("UPDATE account SET amount = " + amountToSubstract);
+                    System.out.println("Balance after withdrawal: " + amountToSubstract);
+                } else {
+                    System.out.println("Not enough funds");
+                }
+                initializePage();
+                break;
+            }
+            default:
+                throw new IllegalArgumentException("Unexpected value: " + userInput);
+        }
+
+    }
 
 
-				ResultSet rs = statement.executeQuery("SELECT * FROM account WHERE name = '" + resultSet.getString("name") + "'" );
-				rs.next();
-				System.out.println("Balance after withdrawal: " + rs.getInt("amount"));
-			} else {
-				System.out.println("Not enough funds");
-			}
-
-			initializePage();
-			break;
-		}
-		default:
-			throw new IllegalArgumentException("Unexpected value: " + userInput);
-		}
-
-	}
-	
-	
 }
