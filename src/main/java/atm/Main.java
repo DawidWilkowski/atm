1715fb57008825f0db7503f5cbe2677f6328e301
DawidWilkowski <71447167+DawidWilkowski@users.noreply.github.com>
2022-11-13 10:43:17 +0100

    added app.properties
    changed password storage type

--- src/main/java/atm/Main.java
+++ src/main/java/atm/Main.java
@@ -1,96 +1,137 @@
 package atm;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 import java.sql.*;
+import java.util.Properties;
 import java.util.Scanner;
+
 
 public class Main {
 
+	Properties prop = new Properties();
 
-    public static void main(String[] args) throws SQLException, ClassNotFoundException {
-        Main main = new Main();
-        Statement statement = main.databaseConnect();
-        Scanner scannerObject = new Scanner(System.in);
+	public static void main(String[] args) throws SQLException, ClassNotFoundException {
+
+		Main main = new Main();
+		Statement statement = main.databaseConnect();
+		Scanner scannerObject = new Scanner(System.in);
 		System.out.println("Enter Login: ");
-        String login = scannerObject.nextLine();
+		String login = scannerObject.nextLine();
 		System.out.println("Enter Password: ");
 		String password = scannerObject.nextLine();
 
-        ResultSet resultSet = statement.executeQuery("SELECT * FROM account WHERE login = '" + login + "' AND password = '" + password + "'");
-        if (resultSet.next() == true) {
-            System.out.println("Hello " + resultSet.getString("login"));
-            main.initializePage();
-            while (true) {
-                main.inputFromUser(resultSet);
-            }
-        } else {
-            throw new RuntimeException("Wrong username or password");
-        }
-    }
+		ResultSet resultSet = statement
+				.executeQuery("SELECT * FROM account WHERE login = '" + login + "' AND password = '" + password + "'");
+		if (resultSet.next() == true) {
+			System.out.println("Hello " + resultSet.getString("login"));
+			main.initializePage();
+			while (true) {
+				main.inputFromUser(resultSet);
+			}
+		} else {
+			throw new RuntimeException("Wrong username or password");
+		}
+	}
 
-    private Statement databaseConnect() throws SQLException {
-        Database database = new Database();
-        Connection connect = DriverManager.getConnection(database.getURL(), database.getUSERNAME(), database.getPASSWORD());
-        Statement statement = connect.createStatement();
-        return statement;
-    }
+	private Statement databaseConnect() throws SQLException {
+		try (InputStream input = new FileInputStream("application.properties")) {
+			prop.load(input);
+			String URL = prop.getProperty("URL");
+			String USERNAME = prop.getProperty("USERNAME");
+			String PASSWORD = prop.getProperty("PASSWORD");
 
+			Connection connect = DriverManager.getConnection(stringFormat(URL), stringFormat(USERNAME),
+					stringFormat(PASSWORD));
+			Statement statement = connect.createStatement();
+			return statement;
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+		return null;
 
-    private void initializePage() {
-        System.out.println("International Bank of Poland");
-        System.out.println("How can we help You?");
-        System.out.println("1 - Balance");
-        System.out.println("2 - Deposit");
-        System.out.println("3 - Withdraw");
-    }
+	}
 
-    private void inputFromUser(ResultSet resultSet) throws SQLException {
-        Scanner scannerObject = new Scanner(System.in);
-        int userInput = scannerObject.nextInt();
-        switch (userInput) {
-            case 1: {
-                Statement statement = databaseConnect();
-                System.out.println("1 - Balance");
-                ResultSet rs = statement.executeQuery("SELECT * FROM account WHERE login = '" + resultSet.getString("login") + "'");
-                rs.next();
-                System.out.println("You have:" + rs.getFloat("amount") + "zl \n");
-                initializePage();
-                break;
-            }
-            case 2: {
-                System.out.println("Enter amount:");
-                int userDepositAmount = scannerObject.nextInt();
-                Statement statement = databaseConnect();
-                ResultSet result = statement.executeQuery("SELECT * FROM account WHERE login = '" + resultSet.getString("login") + "'");
-                result.next();
-                int amountToAdd = result.getInt("amount") + userDepositAmount;
-                statement.executeUpdate("UPDATE account SET amount = " + amountToAdd);
-                System.out.println("Successfully added " + userDepositAmount + "zl to Your account");
-                System.out.println("You have:" + amountToAdd + "zl \n");
-                initializePage();
-                break;
-            }
-            case 3: {
-                System.out.println("Enter amount:");
-                int userWithdrawalAmount = scannerObject.nextInt();
-                Statement statement = databaseConnect();
-                ResultSet result = statement.executeQuery("SELECT * FROM account WHERE login = '" + resultSet.getString("login") + "'");
-                result.next();
-                if (userWithdrawalAmount <= result.getInt("amount")) {
-                    int amountToSubstract = result.getInt("amount") - userWithdrawalAmount;
-                    statement.executeUpdate("UPDATE account SET amount = " + amountToSubstract);
-                    System.out.println("Balance after withdrawal: " + amountToSubstract);
-                } else {
-                    System.out.println("Not enough funds");
-                }
-                initializePage();
-                break;
-            }
-            default:
-                throw new IllegalArgumentException("Unexpected value: " + userInput);
-        }
+	private String stringFormat(String stringToFormat) {
+		return stringToFormat.substring(1, stringToFormat.length() - 1);
+	}
 
-    }
+	private void initializePage() {
+		System.out.println("International Bank of Poland");
+		System.out.println("How can we help You?");
+		System.out.println("1 - Balance");
+		System.out.println("2 - Deposit");
+		System.out.println("3 - Withdraw");
+	}
 
+	private void inputFromUser(ResultSet resultSet) throws SQLException {
+		Scanner scannerObject = new Scanner(System.in);
+		Statement statement = databaseConnect();
+		int userInput = scannerObject.nextInt();
+		switch (userInput) {
+		case 1: {
+
+			System.out.println("1 - Balance");
+			ResultSet rs = statement
+					.executeQuery("SELECT * FROM account WHERE login = '" + resultSet.getString("login") + "'");
+			rs.next();
+			System.out.println("You have:" + rs.getFloat("amount") + "zl \n");
+			initializePage();
+			break;
+		}
+		case 2: {
+			System.out.println("Enter amount:");
+			int userDepositAmount = scannerObject.nextInt();
+			ResultSet result = statement
+					.executeQuery("SELECT * FROM account WHERE login = '" + resultSet.getString("login") + "'");
+			result.next();
+			int amountToAdd = result.getInt("amount") + userDepositAmount;
+			statement.executeUpdate("UPDATE account SET amount = " + amountToAdd);
+			System.out.println("Successfully added " + userDepositAmount + "zl to Your account");
+			System.out.println("You have:" + amountToAdd + "zl \n");
+			initializePage();
+			break;
+		}
+		case 3: {
+			System.out.println("Enter amount:");
+			int userWithdrawalAmount = scannerObject.nextInt();
+			ResultSet result = statement
+					.executeQuery("SELECT * FROM account WHERE login = '" + resultSet.getString("login") + "'");
+			result.next();
+			if (userWithdrawalAmount <= result.getInt("amount")) {
+				int amountToSubstract = result.getInt("amount") - userWithdrawalAmount;
+				statement.executeUpdate("UPDATE account SET amount = " + amountToSubstract);
+				System.out.println("Balance after withdrawal: " + amountToSubstract);
+			} else {
+				System.out.println("Not enough funds");
+			}
+			initializePage();
+			break;
+		}
+		case 4: {
+
+//            	// Setting URL
+//            	String url_str = "https://v6.exchangerate-api.com/v6/YOUR-API-KEY/latest/USD";
+//
+//            	// Making Request
+//            	URL url = new URL(url_str);
+//            	HttpURLConnection request = (HttpURLConnection) url.openConnection();
+//            	request.connect();
+//
+//            	// Convert to JSON
+//            	JsonParser jp = new JsonParser();
+//            	JsonElement root = jp.parse(new InputStreamReader((InputStream) request.getContent()));
+//            	JsonObject jsonobj = root.getAsJsonObject();
+//
+//            	// Accessing object
+//            	String req_result = jsonobj.get("result").getAsString();
+		}
+		default:
+			throw new IllegalArgumentException("Unexpected value: " + userInput);
+		}
+
+	}
 
 }
